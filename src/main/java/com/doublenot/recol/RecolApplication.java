baff572577f908b5544eebb80d9c24ee8c37ede7
heffrey78 <heffrey78@gmail.com>
2022-05-08 07:31:06 -0400

    Initial commit

---
+++ src/main/java/com/doublenot/recol/RecolApplication.java
@@ -0,0 +1,13 @@
+package com.doublenot.recol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecolApplication.class, args);
+	}
+
+}
